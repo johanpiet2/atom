162971ab8c351e52ef939ba9a0a3fd366a4773ee
Johan Pieterse <pieterse.johan3@gmail.com>
2020-06-16 11:19:33 +0200

    Create _printAdvancedSearchTerms.php

---
+++ apps/qubit/modules/search/templates/_printAdvancedSearchTerms.php
@@ -0,0 +1,13 @@
+<h2><?php echo __('Search for') ?></h2>
+
+<div class="section search-terms">
+  <ul>
+    <?php $i = 0 ?>
+    <?php foreach ($queryTerms as $item): ?>
+      <li>
+        <?php echo (0 < $i++) ? '<strong>'.strtoupper($item['operator'])."</strong>\n" : '' ?>
+        <?php echo $item['term'] ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
