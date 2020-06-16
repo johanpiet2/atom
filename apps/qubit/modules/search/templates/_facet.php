0c3b38c57809dffc81988a32fa4b1fab1444c8f9
Johan Pieterse <pieterse.johan3@gmail.com>
2020-06-16 11:19:29 +0200

    Create _facet.php

---
+++ apps/qubit/modules/search/templates/_facet.php
@@ -0,0 +1,66 @@
+<?php if (isset($sf_request->$facet) || (isset($open) && $open
+  && isset($pager->facets[$facet]) && 0 < count($pager->facets[$facet]['terms']))): ?>
+  <section class="facet open">
+<?php else: ?>
+  <section class="facet">
+<?php endif; ?>
+
+  <div class="facet-header">
+    <p><?php echo $label ?></p>
+  </div>
+
+  <div class="facet-body" id="<?php echo $target ?>">
+
+    <ul>
+      <?php if ($facet === 'levels'): ?>
+        <div class="lod-filter btn-group" data-toggle="buttons">
+          <li>
+            <label>
+              <input type="radio" name="lod-filter" data-link="<?php echo $topLvlDescUrl ?>" <?php echo $checkedTopDesc ?>>
+              <?php echo __('Top-level descriptions') ?>
+            </label>
+            <label>
+              <input type="radio" name="lod-filter" data-link="<?php echo $allLvlDescUrl ?>" <?php echo $checkedAllDesc ?>>
+              <?php echo __('All descriptions') ?>
+            </label>
+          </li>
+        </div>
+      <?php endif; ?>
+
+      <?php $filters = sfOutputEscaper::unescape($filters) ?>
+
+      <?php if (!isset($filters[$facet])): ?>
+        <li class="active">
+      <?php else: ?>
+        <li>
+      <?php endif; ?>
+        <?php echo link_to(__('All'), array(
+          $facet => null,
+          'page' => null) + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()) ?>
+      </li>
+
+      <?php if (isset($pager->facets[$facet])): ?>
+        <?php foreach ($pager->facets[$facet]['terms'] as $id => $term): ?>
+          <li <?php if (in_array($id, (array)@$filters[$facet])) echo 'class="active"' ?>>
+            <?php echo link_to(
+              __($term['term']),
+              array(
+                $facet => (
+                  @$filters[$facet]
+                    ?
+                      implode(',', array_diff(
+                        array_merge(@$filters[$facet], array($id)),
+                        array_intersect(@$filters[$facet], array($id))))
+                    :
+                      $id),
+                'page' => null) + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()) ?>
+            <span class="facet-count"><?php echo $term['count'] ?></span>
+          </li>
+        <?php endforeach; ?>
+      <?php endif; ?>
+
+    </ul>
+
+  </div>
+
+</section>
